4e5042c15975c341414379567e2d271206df0fa4
Sam Mason <smason@cloudflare.com>
2021-01-24 23:51:23 +0000

    Add Maalavidaa

--- packages/site/utils/previewNFTs.js
+++ packages/site/utils/previewNFTs.js
@@ -5,4 +5,6 @@ export default [
   { contract: "0xb932a70a57673d89f4acffbe830e8ed7f75fb9e0", tokenId: "18552" },
   // Jaen
   { contract: "0xb6dae651468e9593e4581705a09c10a76ac1e0c8", tokenId: "1058" },
+  // Maalavidaa
+  { contract: "0xb932a70a57673d89f4acffbe830e8ed7f75fb9e0", tokenId: "18218" },
 ]
