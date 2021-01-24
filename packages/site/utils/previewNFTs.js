300778a9f73e5f5946a1a15190c4d64fff16a863
Sam Mason <smason@cloudflare.com>
2021-01-24 23:49:33 +0000

    Re-enable other previews

--- packages/site/utils/previewNFTs.js
+++ packages/site/utils/previewNFTs.js
@@ -1,8 +1,8 @@
 export default [
   // PAK
-  // { contract: "0xb932a70a57673d89f4acffbe830e8ed7f75fb9e0", tokenId: "17824" },
-  // // Arc4G
-  // { contract: "0xb932a70a57673d89f4acffbe830e8ed7f75fb9e0", tokenId: "18552" },
+  { contract: "0xb932a70a57673d89f4acffbe830e8ed7f75fb9e0", tokenId: "17824" },
+  // Arc4G
+  { contract: "0xb932a70a57673d89f4acffbe830e8ed7f75fb9e0", tokenId: "18552" },
   // Jaen
   { contract: "0xb6dae651468e9593e4581705a09c10a76ac1e0c8", tokenId: "1058" },
 ]
