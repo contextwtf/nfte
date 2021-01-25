1387a50155f80f76280362581cf58a884d913be4
Sam Mason <smason@cloudflare.com>
2021-01-25 16:17:52 +0000

    Add fathom snippet

--- packages/site/pages/_app.js
+++ packages/site/pages/_app.js
@@ -1,3 +1,6 @@
+import { useEffect } from "react"
+import { useRouter } from "next/router"
+import * as Fathom from "fathom-client"
 import { css } from "stitches.config"
 
 css.global({
@@ -13,5 +16,24 @@ css.global({
 })
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter()
+  useEffect(() => {
+    // Initialize Fathom when the app loads
+    Fathom.load("CUOXTDGQ", {
+      excludedDomains: ["localhost"],
+    })
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview()
+    }
+    // Record a pageview when route changes
+    router.events.on("routeChangeComplete", onRouteChangeComplete)
+
+    // Unassign event listener
+    return () => {
+      router.events.off("routeChangeComplete", onRouteChangeComplete)
+    }
+  }, [])
+
   return <Component {...pageProps} />
 }
