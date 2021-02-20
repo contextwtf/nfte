f56aea87c8af98d01257fae3920282cb0cbfa785
Sam Mason <smason@cloudflare.com>
2021-02-20 14:39:43 +0000

    Add contracts export from react package

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,4 +1,5 @@
 import { NFTE, css } from "./components/NFTE"
 import Embed from "./components/Embed"
+import contracts from "./contracts"
 
-export { NFTE, Embed, css }
+export { NFTE, Embed, css, contracts }
--- packages/site/utils/previewNFTs.js
+++ packages/site/utils/previewNFTs.js
@@ -1,34 +1,30 @@
-const CONTRACTS = {
-  FOUNDATION: "0x3b3ee1931dc30c1957379fac9aba94d1c48a5405",
-  SUPERRARE: "0xb932a70a57673d89f4acffbe830e8ed7f75fb9e0",
-  ETHBLOCKART: "0xb80fbf6cdb49c33dc6ae4ca11af8ac47b0b4c0f3",
-  ZORA: "0xabefbc9fd2f806065b4f3c237d4b59d9a97bcac7",
-  RARIBLE: "0x60f80121c31a0d46b5279700f9df786054aa5ee5",
-}
+import { contracts } from "@nfte/react"
 
 export default [
   // Arc4G (SuperRare)
-  { contract: CONTRACTS.SUPERRARE, tokenId: "18552" },
+  { contract: contracts.superrare, tokenId: "18552" },
   // Jeff Kraus (Foundation)
-  { contract: CONTRACTS.FOUNDATION, tokenId: "15" },
+  { contract: contracts.foundation, tokenId: "15" },
   // Sam Mason de Caires (Foundation)
-  { contract: CONTRACTS.FOUNDATION, tokenId: "214" },
+  { contract: contracts.foundation, tokenId: "214" },
   // EthBlock.art
-  { contract: CONTRACTS.ETHBLOCKART, tokenId: "149" },
+  { contract: contracts.ethblockart, tokenId: "149" },
   // Zora
-  { contract: CONTRACTS.ZORA, tokenId: "0" },
+  { contract: contracts.zora, tokenId: "0" },
   // James Owen (SuperRare)
-  { contract: CONTRACTS.SUPERRARE, tokenId: "17158" },
+  { contract: contracts.superrare, tokenId: "17158" },
   // Mr.J (Rarible)
-  { contract: CONTRACTS.RARIBLE, tokenId: "78177" },
+  { contract: contracts.rarible, tokenId: "78177" },
   // Audio Ameer Suhayb Carter (Zora)
-  { contract: CONTRACTS.ZORA, tokenId: "178" },
+  { contract: contracts.zora, tokenId: "178" },
   // Audio Ameer Suhayb Carter (Zora)
-  { contract: CONTRACTS.ZORA, tokenId: "178" },
+  { contract: contracts.zora, tokenId: "178" },
   // David Porte Beckfield (Foundation)
-  { contract: CONTRACTS.FOUNDATION, tokenId: "45" },
+  { contract: contracts.foundation, tokenId: "45" },
   // Sean Williams (Foundation)
-  { contract: CONTRACTS.FOUNDATION, tokenId: "301" },
+  { contract: contracts.foundation, tokenId: "301" },
   // Danny Jones (Foundation)
-  { contract: CONTRACTS.FOUNDATION, tokenId: "387" },
+  { contract: contracts.foundation, tokenId: "387" },
+  // Ryan Putname (Foundation)
+  { contract: contracts.foundation, tokenId: "467" },
 ]
